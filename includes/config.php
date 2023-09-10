86648940e9480f18d052d6b05f9493f58737ea5b
Mário Coxe <94186890+Mario-Coxe@users.noreply.github.com>
2023-09-10 02:44:51 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,12 +1,12 @@
 <?php
-define('DB_SERVER','http://135.181.43.100/');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME','newsportal');
-$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+define('DB_SERVER', 'localhost');  // Altere para o endereço do servidor MySQL se não estiver no mesmo servidor
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'newsportal');
+$con = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+
 // Check connection
-if (mysqli_connect_errno())
-{
- echo "Failed to connect to MySQL: " . mysqli_connect_error();
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 ?>
